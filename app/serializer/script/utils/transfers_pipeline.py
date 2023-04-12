94ccf7a890a7874f123a2f60406a7e956a092b37
Gerry <gmijares@savagex.com>
2023-04-12 00:01:45 -0600

    added pipelines

--- app/serializer/script/utils/transfers_pipeline.py
+++ app/serializer/script/utils/transfers_pipeline.py
@@ -12,6 +12,8 @@ class TransferPipeline(object):
         items = self.creditPaymentPipe(items)
         items = self.paymentFromDebitPipe(items)
         items = self.paymentFromCashPipe(items)
+        items = self.transferDebitToSightPipe(items)
+        items = self.transferSightToDebitPipe(items)
         print(items)
         return items 
 
@@ -42,5 +44,17 @@ class TransferPipeline(object):
     def paymentFromDebitPipe(self, items):
         key = 'PAGO POR TRANSFERENCIA'
         newMemo = 'payment-from-debit'
+        items.loc[items.description.str.contains(key),'description'] = newMemo
+        return items
+    
+    def transferDebitToSightPipe(self, items):
+        key = 'CHEQUES A INVERSION VISTA'
+        newMemo = 'transfer-debit-to-sight'
+        items.loc[items.description.str.contains(key),'description'] = newMemo
+        return items
+    
+    def transferSightToDebitPipe(self, items):
+        key = 'INVERSION VISTA A CHEQUES'
+        newMemo = 'transfer-sight-to-debit'
         items.loc[items.description.str.contains(key),'description'] = newMemo
         return items
